495470a9cb2155ab854153d22074b6a1ffb8f6c3
James Michael <jomichael1994@gmail.com>
2025-07-08 15:50:03 +0100

    add solutions

---
+++ sql/6ku/join-and-rank.sql
@@ -0,0 +1,10 @@
+SELECT 
+  p.id, 
+  p.name, 
+  COUNT(s.id) sale_count,
+  RANK() OVER (ORDER BY COUNT(s.id) DESC) AS sale_rank
+FROM people p
+JOIN sales s
+ON p.id = s.people_id
+GROUP BY 1, 2
+ORDER BY 3 DESC;
